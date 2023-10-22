047c67f0429c21dcde7a3addde40629d4628de5c
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-10-22 19:27:31 +0300

    added missed annotation and removed redundant dependencies

--- src/main/java/onlinebookstore/repository/BookRepository.java
+++ src/main/java/onlinebookstore/repository/BookRepository.java
@@ -2,6 +2,8 @@ package onlinebookstore.repository;
 
 import onlinebookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
